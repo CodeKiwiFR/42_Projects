00b9f0cc12a371282e276654039968fab0bd875d
mhotting <madric.hottinger.101@gmail.com>
2023-11-09 17:23:42 +0100

    Completes substr (with tests) - Corrects calloc (not sure about this)

--- 01_LIBFT/libft/ft_calloc.c
+++ 01_LIBFT/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:09:46 by mhotting          #+#    #+#             */
-/*   Updated: 2023/11/09 13:12:10 by mhotting         ###   ########.fr       */
+/*   Updated: 2023/11/09 17:16:09 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	if (size == 0 || nmemb == 0 || (size > 0 && nmemb > 2147483647 / size))
+	if (size > 0 && nmemb > 2147483647 / size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
--- 01_LIBFT/srcs/test_substr.c
+++ 01_LIBFT/srcs/test_substr.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 08:36:04 by mhotting          #+#    #+#             */
-/*   Updated: 2023/11/09 15:59:02 by mhotting         ###   ########.fr       */
+/*   Updated: 2023/11/09 17:21:29 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,71 @@
 
 void	test_substr(void)
 {
-	// int	cpt;
-	// int	test_val;
+	int		cpt;
+	int		test_val;
+	char	*res;
 
-	// cpt = 1;
-	// print_test_intro("substr");
-	// 
-	// // Test 1
-	// test_val = cpt;
-	// print_test_infos(cpt++, test_val, 0);
+	cpt = 1;
+	print_test_intro("substr");
+	
+	// Test 1
+	res = ft_substr("abcdefghi", 0, 4);
+	if (res == NULL)
+		printf("ERROR\n\n");
+	else
+	{
+		test_val = (strcmp(res, "abcd") == 0);
+		print_test_infos(cpt++, test_val, 0);
+		free(res);
+	}
+
+	// Test 2
+	res = ft_substr("abcdefghi", 6, 4);
+	if (res == NULL)
+		printf("ERROR\n\n");
+	else
+	{
+		test_val = (strcmp(res, "ghi") == 0);
+		print_test_infos(cpt++, test_val, 0);
+		free(res);
+	}
+
+	// Test 3
+	res = ft_substr("abcdefghi", 15, 20);
+	test_val = res == NULL;
+	print_test_infos(cpt++, test_val, 0);
+
+	// Test 4
+	res = ft_substr("abcdefghi", 2, 100);
+	if (res == NULL)
+		printf("ERROR\n\n");
+	else
+	{
+		test_val = (strcmp(res, "cdefghi") == 0);
+		print_test_infos(cpt++, test_val, 0);
+		free(res);
+	}
+
+	// Test 5
+	res = ft_substr("abcdefghi", 0, 0);
+	if (res == NULL)
+		printf("ERROR\n\n");
+	else
+	{
+		test_val = (strcmp(res, "") == 0);
+		print_test_infos(cpt++, test_val, 0);
+		free(res);
+	}
+
+	// Test 6
+	res = ft_substr("abcdefghi", 7, 50);
+	if (res == NULL)
+		printf("ERROR\n\n");
+	else
+	{
+		test_val = (strcmp(res, "hi") == 0);
+		print_test_infos(cpt++, test_val, 1);
+		free(res);
+	}
+
 }
