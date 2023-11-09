dc457429a4d5b6b1bf5b1ecbe221de7772f19e69
mhotting <madric.hottinger.101@gmail.com>
2023-11-09 15:04:52 +0100

    Adds tests to calloc and stdup

--- 01_LIBFT/srcs/test_memmove.c
+++ 01_LIBFT/srcs/test_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:07:32 by mhotting          #+#    #+#             */
-/*   Updated: 2023/11/08 10:51:59 by mhotting         ###   ########.fr       */
+/*   Updated: 2023/11/09 14:24:39 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,16 @@ void	test_memmove(void)
 	bzero(ptr3, 10);
 	memcpy(ptr1, "abcdefghi", 9);
 	memcpy(ptr3, "abcdefghi", 9);
+
+	// Test 0 (no overlap)
+	char s[] = {65, 66, 67, 68, 69, 0, 45};
+	char s0[] = { 0,  0,  0,  0,  0,  0, 0};
+	char s2[] = {65, 66, 67, 68, 69, 0, 45};
+	char s02[] = { 0,  0,  0,  0,  0,  0, 0};
+	memmove(s0, s, 7);
+	ft_memmove(s02, s2, 7);
+	test_result = memcmp(s2, s02, 7);
+	print_test_infos(cpt++, test_result == 0, 0);
 
 	// Test 1 (no overlap)
 	memmove(ptr2, ptr1, 3);
--- 01_LIBFT/srcs/test_strncmp.c
+++ 01_LIBFT/srcs/test_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 09:22:00 by mhotting          #+#    #+#             */
-/*   Updated: 2023/11/09 10:37:34 by mhotting         ###   ########.fr       */
+/*   Updated: 2023/11/09 14:46:12 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,31 @@ void	test_strncmp(void)
 	// Test 1
 	lib_res = strncmp("Hello", "Hello", 5);
 	my_res = ft_strncmp("Hello", "Hello", 5);
-	test_result = lib_res == my_res;
+	test_result = (lib_res == 0 && lib_res == my_res);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 2
 	lib_res = strncmp("Hello", "Hello", 0);
 	my_res = ft_strncmp("Hello", "Hello", 0);
-	test_result = lib_res == my_res;
+	test_result = (lib_res == 0 && lib_res == my_res);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 3
 	lib_res = strncmp("Hello", "Heldi", 3);
 	my_res = ft_strncmp("Hello", "Heldi", 3);
-	test_result = lib_res == my_res;
+	test_result = (lib_res == 0 && lib_res == my_res);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 4
 	lib_res = strncmp("Hello", "Heldi", 4);
 	my_res = ft_strncmp("Hello", "Heldi", 4);
-	test_result = lib_res == my_res;
+	test_result = (lib_res > 0 && my_res > 0);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 5
-	lib_res = strncmp("Hello", "Hel", 3);
-	my_res = ft_strncmp("Hello", "Hel", 3);
-	test_result = lib_res == my_res;
+	lib_res = strncmp("Hel", "Hello", 4);
+	my_res = ft_strncmp("Hel", "Hello", 4);
+	test_result = (lib_res < 0 && my_res < 0);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 6
@@ -59,7 +59,7 @@ void	test_strncmp(void)
 	s2 = "";
 	lib_res = strncmp(s1, s2, 50);
 	my_res = ft_strncmp(s1, s2, 50);
-	test_result = lib_res == my_res;
+	test_result = (lib_res > 0 && my_res > 0);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 7
@@ -67,24 +67,24 @@ void	test_strncmp(void)
 	s2 = "Hello";
 	lib_res = strncmp(s1, s2, 100);
 	my_res = ft_strncmp(s1, s2, 100);
-	test_result = lib_res == my_res;
+	test_result = (lib_res < 0 && my_res < 0);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 8
 	lib_res = strncmp("", "", 5);
 	my_res = ft_strncmp("", "", 5);
-	test_result = lib_res == my_res;
+	test_result = (lib_res == 0 && lib_res == my_res);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 9
 	lib_res = strncmp("Hello", "Lol", 0);
 	my_res = ft_strncmp("Hello", "Lol", 0);
-	test_result = lib_res == my_res;
+	test_result = (lib_res == 0 && lib_res == my_res);
 	print_test_infos(cpt++, test_result, 0);
 
 	// Test 10
-	lib_res = strncmp("Hello", "Zello", 6);
-	my_res = ft_strncmp("Hello", "Zello", 6);
-	test_result = lib_res == my_res;
+	lib_res = strncmp("Hello", "Aello", 6);
+	my_res = ft_strncmp("Hello", "Aello", 6);
+	test_result = (lib_res > 0 && my_res > 0);
 	print_test_infos(cpt++, test_result, 1);
 }
