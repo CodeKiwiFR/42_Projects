f2c60a300027309545fbf6cc19f5e3998019161f
mhotting <madric.hottinger.101@gmail.com>
2023-11-09 19:41:46 +0100

    Completes ft_putendl_fd (not tested yet)

--- 01_LIBFT/libft/ft_putendl_fd.c
+++ 01_LIBFT/libft/ft_putendl_fd.c
@@ -6,8 +6,14 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:11:59 by mhotting          #+#    #+#             */
-/*   Updated: 2023/11/06 18:21:24 by mhotting         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:37:34 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putendl_fd(char *s, int fd);
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
