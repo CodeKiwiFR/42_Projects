84fa9f7511aef6bcc74c0e5820aa747120af5a39
mhotting <madric.hottinger.101@gmail.com>
2023-11-21 13:41:46 +0100

    GNL: Adds ability to read from stdin if no arg is passed to the program

--- 02_GET_NEXT_LINE/main.c
+++ 02_GET_NEXT_LINE/main.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:05:07 by mhotting          #+#    #+#             */
-/*   Updated: 2023/11/21 09:21:12 by mhotting         ###   ########.fr       */
+/*   Updated: 2023/11/21 12:18:24 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,16 @@ int	main(int argc, char **argv)
 	int		count;
 	char	*read_line;
 
-	if (argc == 0)
-		return (-1);
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	if (argc == 1)
+		fd = 0;
+	else
 	{
-		printf("ERROR - Could not open given file\n");
-		return (-1);
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			printf("ERROR - Could not open given file\n");
+			return (-1);
+		}
 	}
 	count = 0;
 	read_line = get_next_line(fd);
@@ -41,6 +44,7 @@ int	main(int argc, char **argv)
 		free(read_line);
 		read_line = get_next_line(fd);
 	}
-	close(fd);
+	if (fd != 0)
+		close(fd);
 	return (0);
 }
