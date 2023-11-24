3e3d3448c66e17044b26e6d1ee6e2e2fc389fa26
CodeKiwiFR <madric.hottinger.101@gmail.com>
2023-11-24 22:42:01 +0100

    Structuring ft_printf function

--- 03_FT_PRINTF/srcs/ft_printf.c
+++ 03_FT_PRINTF/srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 08:41:46 by mhotting          #+#    #+#             */
-/*   Updated: 2023/11/23 15:44:30 by mhotting         ###   ########.fr       */
+/*   Updated: 2023/11/24 17:21:54 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,26 @@
 int	ft_printf(const char *format, ...)
 {
 	va_list				args;
-	size_t				i;
 	t_format_specifier	*selector;
 	int					count;
 	
+	selector = init_format_specifier();
+	if (selector == NULL)
+		return (0);
 	va_start(args, format);
 	count = 0;
-	selector = NULL;
-	i = 0;
-	while (format[i] != '\0')
+	while (*format != '\0')
 	{
-		ft_putchar_fd(format[i], 1);
-		i++;
+		if (*format == '%')
+			count += print_formatted_content(format, args, selector);
+		else
+		{
+			ft_putchar_fd(*format, 1);
+			count++;
+		}
+		format++;
 	}
+	free_format_specifier(selector);
+	va_end(args);
 	return (count);
 }
