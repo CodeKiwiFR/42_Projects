06aacb8fe60c254ac01aa71edc99c1ec3dbd3059
mhotting <madric.hottinger.101@gmail.com>
2024-02-18 15:18:55 +0100

    Adds ft_strtok.c to libft

--- LIBFT/includes/libft.h
+++ LIBFT/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:29:33 by mhotting          #+#    #+#             */
-/*   Updated: 2024/02/16 14:49:37 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/02/18 14:56:18 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,7 @@ int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 char			*ft_strrchr(const char *s, int c);
+char			*ft_strtok(char *str, const char *delim);
 char			*ft_strtrim(char const *s1, char const *set);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 int				ft_tolower(int c);
